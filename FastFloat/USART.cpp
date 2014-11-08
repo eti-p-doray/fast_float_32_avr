c55dea0cde9d7f7cab5e2bfae295dd7a4ca1fc44
Etienne Pierre-Doray <etipdoray@gmail.com>
2014-11-08 12:53:19 -0500

    Allows to send fast_float objects

--- FastFloat/USART.cpp
+++ FastFloat/USART.cpp
@@ -8,16 +8,27 @@
 
 #include "USART.h"
 #include <avr/io.h>
+#include <stdlib.h>
 
-void USART::init() {
-    UCSR0A = 0x02;
-    UCSR0B = 0x18;
-    UCSR0C = 0x06;
-    UBRR0H = 0x00;
-    UBRR0L = 0x67;
+
+
+void USART::init(uint32_t baud) {
+    UCSR0A |= (1<<U2X0);
+    UCSR0B |= (1<<TXEN0) ;
+    UCSR0C |= (1<<UCSZ00) | (1<<UCSZ01);
+    
+    UBRR0 = int16_t( F_CPU/(8*baud) - 1 );
 }
 
 void USART::write(uint8_t newdata) {
     while(!(UCSR0A&(1<<UDRE0)));
     UDR0 = newdata;
+}
+
+
+void USART::write(FastFloat data) {
+    USART::write(uint8_t(data.get()>>24));
+    USART::write(uint8_t(data.get()>>16));
+    USART::write(uint8_t(data.get()>>8));
+    USART::write(uint8_t(data.get()));
 }
