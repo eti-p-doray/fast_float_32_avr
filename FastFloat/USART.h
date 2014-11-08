53ab1fd4de440924657259d4505b20420f786fec
Etienne Pierre-Doray <etipdoray@gmail.com>
2014-11-08 12:52:54 -0500

    Allows to send fast_float objects

--- FastFloat/USART.h
+++ FastFloat/USART.h
@@ -6,16 +6,21 @@
 //
 //
 
-#ifndef float_32_USART_h
-#define float_32_USART_h
+#ifndef USART_h
+#define USART_h
 
 #include "stdint.h"
 #include <avr/io.h>
 
+#include "FastFloat.h"
+
+using namespace fast_float_32_avr;
+
 class USART {
 public:
-    static void init();
+    static void init(uint32_t baud);
     static void write(uint8_t newdata);
+    static void write(FastFloat data);
 };
 
 #endif
