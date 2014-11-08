7e10eef2bb745c6d6a56a26fda8fb4e7646d5080
Etienne Pierre-Doray <etipdoray@gmail.com>
2014-11-08 12:48:09 -0500

    Update FastFloat.h

--- FastFloat/FastFloat.h
+++ FastFloat/FastFloat.h
@@ -6,8 +6,8 @@
 //
 //
 
-#ifndef FastFloat_FastFloat_h
-#define FastFloat_FastFloat_h
+#ifndef FastFloat_h
+#define FastFloat_h
 #include "stdint.h"
 #include <avr/io.h>
 
@@ -16,135 +16,70 @@ namespace fast_float_32_avr {
 class FastFloat {
 public:
     inline FastFloat() {};//Default constructor
-    inline FastFloat(const float value);//Constructor which takes a float type
-    inline FastFloat(const int32_t value);//Constructor which takes a int32_t type
-    inline FastFloat(const int16_t value);//Constructor which takes a int16_t type
-    inline FastFloat(const int8_t value);//Constructor which takes a int8_t type
-    inline FastFloat(const uint8_t nexp, const uint8_t nlsb, const int16_t nmsb);//Constructor which takes raw bytes
+    inline FastFloat(const double value);//Constructor which takes a double type
     
-    inline operator float();//Conversion operator to float type
-    inline operator int32_t();//Conversion operator to int32_t type
-    inline operator int16_t();//Conversion operator to int16_t type
-    inline operator int8_t();//Conversion operator to int8_t type
+    inline operator double();//Conversion operator to double type
     
     inline FastFloat& operator=(const FastFloat value);
-    inline FastFloat& operator=(const float value);
-    inline FastFloat& operator=(const int32_t value);
-    inline FastFloat& operator=(const int16_t value);
-    inline FastFloat& operator=(const int8_t value);
+    inline FastFloat& operator=(const double value);
  
     inline int32_t get();
     
-    friend FastFloat operator+(const FastFloat op1, const FastFloat op2);
-    inline friend FastFloat operator-(const FastFloat op1, FastFloat op2);
-    friend FastFloat operator*(const FastFloat op1, const FastFloat op2);
-    friend FastFloat operator/(const FastFloat op1, const FastFloat op2);
+    friend FastFloat operator+(const FastFloat a, const FastFloat b);
+    inline FastFloat operator-(FastFloat b);
+    inline FastFloat operator-();
+    friend FastFloat operator*(const FastFloat a, const FastFloat b);
+    friend FastFloat operator/(const FastFloat a, const FastFloat b);
+    friend FastFloat Inverse(const FastFloat a);
     friend FastFloat square(const FastFloat op);
     
-    inline FastFloat operator+=(const FastFloat op2);
-    inline FastFloat operator-=(const FastFloat op2);
-    inline FastFloat operator*=(const FastFloat op2);
-    bool operator==(const FastFloat op2);
-    bool operator<(const FastFloat op2);
-    bool operator<=(const FastFloat op2);
-    bool operator>(const FastFloat op2);
-    bool operator>=(const FastFloat op2);
-    
-    FastFloat abs();
-    FastFloat ldexp(const int8_t power);
+    inline FastFloat operator+=(const FastFloat b);
+    inline FastFloat operator-=(const FastFloat b);
+    inline FastFloat operator*=(const FastFloat b);
+
     FastFloat pow(const uint8_t power);
-    FastFloat sqrt();
-    FastFloat log();
-    
-    FastFloat sin();
-    FastFloat cos();
-    FastFloat arcsin();
-    FastFloat arccos();
     
 private:
-    uint8_t exp;
-    uint8_t lsb;
-    int16_t msb;
+    double data;
     
-    void toFastFloat(float value);
-    void toFastFloat(int32_t value);
-    void toFastFloat(int16_t value);
-    void toFastFloat(int8_t value);
-    
-    float tofloat();
-    int32_t toint32_t();
-    int16_t toint16_t();
-    int8_t toint8_t();
-    
-    static FastFloat sqrtval[2];
+    inline void toFastFloat(double value);
+    inline double tofloat();
 };
     
-    FastFloat operator-(const FastFloat op1, FastFloat op2) {
-        op2.msb -= 0x8000;
-        return op1+op2;
+    double FastFloat::tofloat() {
+        return data;
     }
     
-    FastFloat::FastFloat(const float value) {
+    void FastFloat::toFastFloat(double value) {
+        data = value;
+    }
+
+    
+    FastFloat FastFloat::operator-(FastFloat b) {
+        b = -b;
+        return *this+b;
+    }
+    
+    FastFloat FastFloat::operator-() {
+        FastFloat ans;
+        ans.data = -data;
+        return ans;
+    }
+    
+    FastFloat::FastFloat(const double value) {
         toFastFloat(value);
     }
     
-    FastFloat::FastFloat(const int32_t value) {
-        toFastFloat(value);
-    }
-    
-    FastFloat::FastFloat(const int16_t value) {
-        toFastFloat(value);
-    }
-    
-    FastFloat::FastFloat(const int8_t value) {
-        toFastFloat(value);
-    }
-    
-    FastFloat::FastFloat(const uint8_t nexp, const uint8_t nlsb, const int16_t nmsb) {
-        exp = nexp;
-        lsb = nlsb;
-        msb = nmsb;
-    }
-    
-    FastFloat::operator float() {
+    FastFloat::operator double() {
         return tofloat();
     }
     
-    FastFloat::operator int32_t() {
-        return toint32_t();
-    }
-    
-    FastFloat::operator int16_t() {
-        return toint16_t();
-    }
-    
-    FastFloat::operator int8_t() {
-        return toint8_t();
-    }
-    
     FastFloat& FastFloat::operator=(const FastFloat value) {
-        exp = value.exp;
-        lsb = value.lsb;
-        msb = value.msb;
+        data = value.data;
         return *this;
     }
     
-    FastFloat& FastFloat::operator=(const float value) {
-        toFastFloat(value);
-        return *this;
-    }
-    
-    FastFloat& FastFloat::operator=(const int32_t value) {
-        toFastFloat(value);
-        return *this;
-    }
-    
-    FastFloat& FastFloat::operator=(const int16_t value) {
-        toFastFloat(value);
-        return *this;
-    }
-    
-    FastFloat& FastFloat::operator=(const int8_t value) {
+    FastFloat& FastFloat::operator=(const double value) {
         toFastFloat(value);
         return *this;
     }
@@ -153,19 +88,36 @@ private:
         return *(int32_t*)this;
     }
     
-    FastFloat FastFloat::operator+=(const FastFloat op2) {
-        *this = *this + op2;
+    FastFloat FastFloat::operator+=(const FastFloat b) {
+        *this = *this + b;
         return *this;
     }
     
-    FastFloat FastFloat::operator-=(const FastFloat op2) {
-        *this = *this - op2;
+    FastFloat FastFloat::operator-=(const FastFloat b) {
+        *this = *this - b;
         return *this;
     }
     
-    FastFloat FastFloat::operator*=(const FastFloat op2) {
-        *this = *this * op2;
+    FastFloat FastFloat::operator*=(const FastFloat b) {
+        *this = *this * b;
         return *this;
+    }
+    
+    FastFloat FastFloat::pow(const uint8_t power) {
+        FastFloat cur = *this;
+        FastFloat ans = 0.0f;
+        uint8_t j = 0xfe;
+        for (uint8_t i=0x01; i<=0x80; i<<=1, j-=i) {
+            if (power&i) {
+                ans *= cur;
+            }
+            if ((power&j)==0) {
+                break;
+            }
+            cur = square(cur);
+        }
+        return ans;
+        
     }
     
 }  // namespace fast_float_32_avr
